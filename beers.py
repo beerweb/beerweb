6fa648168ee081e46aa4567856839dbf0855b27d
Joe Meszar <mesz@outk.st>
2015-10-08 23:30:33 -0400

    Added ID property to the beer list. Configured the CSS for the beer table. Next change will be to make the input boxes just be an "Add to cart" button and create a new table of all the items the user has selected.

--- beers.py
+++ beers.py
@@ -1,6 +1,7 @@
 # This is a simple, hard coded list of beer objects that we will pull from
 beers = [
 	{
+		"id": "001",
 		"brewery": "Angry Orchard (Boston Brewing)",
 		"product": "Crisp Apple",
 		"style": "Cider",
@@ -8,6 +9,7 @@ beers = [
 		"price": "4.00"
 	},
 	{
+		"id": "002",
 		"brewery": "Guinness Ltd",
 		"product": "Smithwick's Irish Ale",
 		"style": "Irish Red Ale",
@@ -15,6 +17,7 @@ beers = [
 		"price": "4.50"
 	},
 	{
+		"id": "003",
 		"brewery": "Hoppin' Frog Brewery",
 		"product": "Silk Porter",
 		"style": "American Porter",
@@ -22,6 +25,7 @@ beers = [
 		"price": "5.50"
 	},
 	{
+		"id": "004",
 		"brewery": "Magic Hat",
 		"product": "Magic Hat #9",
 		"style": "Fruit Beer",
@@ -29,6 +33,7 @@ beers = [
 		"price": "4.00"
 	},
 	{
+		"id": "005",
 		"brewery": "Penn Brewery",
 		"product": "Oktoberfest",
 		"style": "Oktoberfest",
@@ -36,6 +41,7 @@ beers = [
 		"price": "4.50"
 	},
 	{
+		"id": "006",
 		"brewery": "Rivertowne",
 		"product": "Maxwell's Scottish Ale",
 		"style": "Scottish Ale",
@@ -43,6 +49,7 @@ beers = [
 		"price": "4.50"
 	},
 	{
+		"id": "007",
 		"brewery": "Victory",
 		"product": "Braumeister Pils",
 		"style": "Pilsner",
@@ -50,6 +57,7 @@ beers = [
 		"price": "3.50"
 	},
 	{
+		"id": "008",
 		"brewery": "Weihenstephaner",
 		"product": "Vitus",
 		"style": "Weizenbock",
@@ -57,6 +65,7 @@ beers = [
 		"price": "7.00"
 	},
 	{
+		"id": "009",
 		"brewery": "Anchor Brewing",
 		"product": "Anchor Humming Ale",
 		"style": "American Pale Ale",
@@ -64,6 +73,7 @@ beers = [
 		"price": "4.50"
 	},
 	{
+		"id": "010",
 		"brewery": "Ayinger",
 		"product": "Brau-Weisse",
 		"style": "Hefeweizen",
@@ -71,6 +81,7 @@ beers = [
 		"price": "7.50"
 	},
 	{
+		"id": "011",
 		"brewery": "Ayinger",
 		"product": "Oktober Fest-Marzen",
 		"style": "Marzen / Oktoberfest",
@@ -78,6 +89,7 @@ beers = [
 		"price": "5.00"
 	},
 	{
+		"id": "012",
 		"brewery": "Goose Island Beer Company",
 		"product": "Bourbon Country Brand Stout",
 		"style": "Imperial Stout",
@@ -85,6 +97,7 @@ beers = [
 		"price": "7.50"
 	},
 	{
+		"id": "013",
 		"brewery": "Great Lakes",
 		"product": "Burning River Pale Ale",
 		"style": "Pale Ale",
@@ -92,6 +105,7 @@ beers = [
 		"price": "6.50"
 	},
 	{
+		"id": "014",
 		"brewery": "Maui Brewing",
 		"product": "CoCoNut Porter",
 		"style": "American Porter",
@@ -99,6 +113,7 @@ beers = [
 		"price": "7.50"
 	},
 	{
+		"id": "015",
 		"brewery": "Mike's",
 		"product": "Boddingtons Brew",
 		"style": "Bitter",
@@ -106,6 +121,7 @@ beers = [
 		"price": "8.00"
 	},
 	{
+		"id": "016",
 		"brewery": "North Country Brewing",
 		"product": "Station 33 Firehouse Red",
 		"style": "Irish Red Ale",
@@ -113,6 +129,7 @@ beers = [
 		"price": "7.00"
 	},
 	{
+		"id": "017",
 		"brewery": "Unibroue",
 		"product": "La Fin Du Monde",
 		"style": "Tripel",
@@ -120,6 +137,7 @@ beers = [
 		"price": "5.00"
 	},
 	{
+		"id": "018",
 		"brewery": "Ayinger",
 		"product": "Celebrator",
 		"style": "Doppelbock",
@@ -127,6 +145,7 @@ beers = [
 		"price": "5.50"
 	},
 	{
+		"id": "019",
 		"brewery": "Brooklyn Brewery",
 		"product": "Winter Ale",
 		"style": "Scottish Ale",
@@ -134,6 +153,7 @@ beers = [
 		"price": "5.00"
 	},
 	{
+		"id": "020",
 		"brewery": "Brooklyn Brewery",
 		"product": "Lager",
 		"style": "American Amber",
@@ -141,6 +161,7 @@ beers = [
 		"price": "5.00"
 	},
 	{
+		"id": "021",
 		"brewery": "Cigar City",
 		"product": "Maduro Oatmeal Brown Ale",
 		"style": "English Brown Ale",
@@ -148,6 +169,7 @@ beers = [
 		"price": "6.00"
 	},
 	{
+		"id": "022",
 		"brewery": "Firestone Walker",
 		"product": "Parabola",
 		"style": "Russian Imperial Stout",
@@ -155,6 +177,7 @@ beers = [
 		"price": "5.00"
 	},
 	{
+		"id": "023",
 		"brewery": "Great Lakes",
 		"product": "Conways Irish Ale",
 		"style": "Irish Red Ale",
@@ -162,20 +185,23 @@ beers = [
 		"price": "4.00"
 	},
 	{
+		"id": "024",
 		"brewery": "Harpoon",
 		"product": "UFO Hefeweizen",
-		"style": "American Pale Wheat",
+		"style": "American Pale Wheat Ale",
 		"abv": "4.80",
 		"price": "3.50"
 	},
 	{
+		"id": "025",
 		"brewery": "Warsteiner Brauerei",
 		"product": "Warsteiner Premium Verum",
-		"style": "German Pilsener",
+		"style": "German Pilsner",
 		"abv": "4.80",
 		"price": "4.00"
 	},
 	{
+		"id": "026",
 		"brewery": "Bell's",
 		"product": "Oberon",
 		"style": "American Pale Wheat Ale",
@@ -183,6 +209,7 @@ beers = [
 		"price": "4.50"
 	},
 	{
+		"id": "027",
 		"brewery": "Carlsberg-Tetley Brewing",
 		"product": "Tetley's English Ale",
 		"style": "English Pale Ale",
@@ -190,6 +217,7 @@ beers = [
 		"price": "8.00"
 	},
 	{
+		"id": "028",
 		"brewery": "Flying Dog",
 		"product": "Double Dog Double Pale Ale (Nitro)",
 		"style": "American Strong Ale",
@@ -197,6 +225,7 @@ beers = [
 		"price": "8.00"
 	},
 	{
+		"id": "029",
 		"brewery": "Flying Dog",
 		"product": "Dogtoberfest",
 		"style": "Oktoberfest",
@@ -204,6 +233,7 @@ beers = [
 		"price": "9.50"
 	},
 	{
+		"id": "030",
 		"brewery": "Great Lakes",
 		"product": "Dortmunder Gold",
 		"style": "Dortmunder Lager",
@@ -211,6 +241,7 @@ beers = [
 		"price": "3.50"
 	},
 	{
+		"id": "031",
 		"brewery": "Leffe",
 		"product": "Brune",
 		"style": "Belgian Dark Ale",
@@ -218,6 +249,7 @@ beers = [
 		"price": "8.00"
 	},
 	{
+		"id": "032",
 		"brewery": "New Belgium Brewing",
 		"product": "Fat Tire Amber",
 		"style": "Amber Ale",
@@ -225,6 +257,7 @@ beers = [
 		"price": "6.50"
 	},
 	{
+		"id": "033",
 		"brewery": "Rust Belt",
 		"product": "McPoyle's Milk Stout",
 		"style": "Sweet Stout",
@@ -232,6 +265,7 @@ beers = [
 		"price": "6.00"
 	},
 	{
+		"id": "034",
 		"brewery": "Straub Brewery",
 		"product": "American Amber (Special Dark)",
 		"style": "American Amber",
@@ -239,6 +273,7 @@ beers = [
 		"price": "3.50"
 	},
 	{
+		"id": "035",
 		"brewery": "Voodoo Brewery",
 		"product": "Cowbell Imperial Oatmeal Milk Stout",
 		"style": "Milk / Sweet Stout",
@@ -246,6 +281,7 @@ beers = [
 		"price": "4.00"
 	},
 	{
+		"id": "036",
 		"brewery": "Anheuser-Busch",
 		"product": "Shock Top",
 		"style": "Belgian White",
@@ -253,6 +289,7 @@ beers = [
 		"price": "3.00"
 	},
 	{
+		"id": "037",
 		"brewery": "DuClaw",
 		"product": "Sweet Baby Jesus Chocolate Peanut Butter Porter",
 		"style": "American Porter",
@@ -260,6 +297,7 @@ beers = [
 		"price": "4.00"
 	},
 	{
+		"id": "038",
 		"brewery": "East End Brewery",
 		"product": "Monkey Boy",
 		"style": "Hefeweizen",
@@ -267,6 +305,7 @@ beers = [
 		"price": "9.00"
 	},
 	{
+		"id": "039",
 		"brewery": "Rivertowne",
 		"product": "Hala Kahiki Pineapple",
 		"style": "Fruit Beer",
@@ -274,6 +313,7 @@ beers = [
 		"price": "8.50"
 	},
 	{
+		"id": "040",
 		"brewery": "Spaten-Franziskaner-Brau",
 		"product": "Spaten Munchner Hell (Premium Lager)",
 		"style": "Munich Helles Lager",
@@ -281,6 +321,7 @@ beers = [
 		"price": "8.00"
 	},
 	{
+		"id": "041",
 		"brewery": "Spring House",
 		"product": "Big Gruesome Peanut Butter Chocolate Stout",
 		"style": "American Imperial Stout",
@@ -288,6 +329,7 @@ beers = [
 		"price": "7.00"
 	},
 	{
+		"id": "042",
 		"brewery": "Straub Brewery",
 		"product": "American Light Lager",
 		"style": "Pale Lager",
@@ -295,6 +337,7 @@ beers = [
 		"price": "3.00"
 	},
 	{
+		"id": "043",
 		"brewery": "Boulder",
 		"product": "Shake Chocolate Porter",
 		"style": "American Porter",
@@ -302,6 +345,7 @@ beers = [
 		"price": "6.00"
 	},
 	{
+		"id": "044",
 		"brewery": "Brooklyn Brewery",
 		"product": "Brown Ale",
 		"style": "American Brown Ale",
@@ -309,13 +353,15 @@ beers = [
 		"price": "4.00"
 	},
 	{
+		"id": "045",
 		"brewery": "Budejovicky Budvar",
 		"product": "Czechvar / Budvar",
-		"style": "Czech Pilsener",
+		"style": "Czech Pilsner",
 		"abv": "5.00",
 		"price": "8.00"
 	},
 	{
+		"id": "046",
 		"brewery": "Clown Shoes",
 		"product": "Brown Angel",
 		"style": "American Double Brown Ale",
@@ -323,6 +369,7 @@ beers = [
 		"price": "6.00"
 	},
 	{
+		"id": "047",
 		"brewery": "Duvel Moortgat",
 		"product": "Maredsous 8 (Brune)",
 		"style": "Dubbel",
@@ -330,6 +377,7 @@ beers = [
 		"price": "5.00"
 	},
 	{
+		"id": "048",
 		"brewery": "Goose Island Beer Company",
 		"product": "312 Urban Wheat",
 		"style": "American Pale Wheat Ale",
@@ -337,6 +385,7 @@ beers = [
 		"price": "6.00"
 	},
 	{
+		"id": "049",
 		"brewery": "Guinness Ltd",
 		"product": "Guinness Draught",
 		"style": "Irish Dry Stout",
@@ -344,6 +393,7 @@ beers = [
 		"price": "4.50"
 	},
 	{
+		"id": "050",
 		"brewery": "Diageo",
 		"product": "Smithwick's Ale",
 		"style": "Irish Ale",
@@ -351,6 +401,7 @@ beers = [
 		"price": "7.00"
 	},
 	{
+		"id": "051",
 		"brewery": "East End Brewery",
 		"product": "Fat Gary Nut Brown Ale",
 		"style": "English Brown Ale",
@@ -358,6 +409,7 @@ beers = [
 		"price": "4.00"
 	},
 	{
+		"id": "052",
 		"brewery": "Hoegaarden",
 		"product": "Hoegaarden",
 		"style": "Belgian White",
@@ -365,6 +417,7 @@ beers = [
 		"price": "4.00"
 	},
 	{
+		"id": "053",
 		"brewery": "Kronenbourg",
 		"product": "Kronenbourg 1664",
 		"style": "Pale Lager",
@@ -372,6 +425,7 @@ beers = [
 		"price": "8.50"
 	},
 	{
+		"id": "054",
 		"brewery": "Miller Coors",
 		"product": "Miller Lite",
 		"style": "Pale Lager",
@@ -379,6 +433,7 @@ beers = [
 		"price": "3.00"
 	},
 	{
+		"id": "055",
 		"brewery": "Rogue",
 		"product": "Hazelnut Brown Nectar Ale",
 		"style": "American Brown Ale",
@@ -386,6 +441,7 @@ beers = [
 		"price": "6.00"
 	},
 	{
+		"id": "056",
 		"brewery": "Rolling Rock",
 		"product": "Rock Green Light",
 		"style": "Pale Lager",
@@ -393,6 +449,7 @@ beers = [
 		"price": "4.50"
 	},
 	{
+		"id": "057",
 		"brewery": "Rolling Rock",
 		"product": "Extra Pale",
 		"style": "American Lager",
@@ -400,6 +457,7 @@ beers = [
 		"price": "8.50"
 	},
 	{
+		"id": "058",
 		"brewery": "Samuel Adams",
 		"product": "Noble Pils",
 		"style": "Czech Pilsner",
@@ -407,6 +465,7 @@ beers = [
 		"price": "9.50"
 	},
 	{
+		"id": "059",
 		"brewery": "Stella Artois",
 		"product": "Stella Artois",
 		"style": "Euro Pale Lager",
@@ -414,6 +473,7 @@ beers = [
 		"price": "6.00"
 	},
 	{
+		"id": "060",
 		"brewery": "SweetWater Brewing Company",
 		"product": "Georgia Brown",
 		"style": "English Brown Ale",
@@ -421,6 +481,7 @@ beers = [
 		"price": "7.50"
 	},
 	{
+		"id": "061",
 		"brewery": "Bass",
 		"product": "Bass Ale",
 		"style": "English Pale Ale",
@@ -428,6 +489,7 @@ beers = [
 		"price": "5.50"
 	},
 	{
+		"id": "062",
 		"brewery": "Bear Republic",
 		"product": "Racer 5",
 		"style": "American IPA",
@@ -435,6 +497,7 @@ beers = [
 		"price": "8.00"
 	},
 	{
+		"id": "063",
 		"brewery": "East End Brewery",
 		"product": "Oatmeal Stout",
 		"style": "Oatmeal Stout",
@@ -442,6 +505,7 @@ beers = [
 		"price": "8.00"
 	},
 	{
+		"id": "064",
 		"brewery": "Founders",
 		"product": "Oatmeal Stout (Nitro)",
 		"style": "Oatmeal Stout",
@@ -449,6 +513,7 @@ beers = [
 		"price": "8.50"
 	},
 	{
+		"id": "065",
 		"brewery": "Guinness Ltd",
 		"product": "Harp Lager",
 		"style": "Euro Pale Lager",
@@ -456,6 +521,7 @@ beers = [
 		"price": "5.00"
 	},
 	{
+		"id": "066",
 		"brewery": "North Coast Brewing",
 		"product": "Old Stock Ale",
 		"style": "Old Ale",
@@ -463,6 +529,7 @@ beers = [
 		"price": "8.50"
 	},
 	{
+		"id": "067",
 		"brewery": "Samuel Adams",
 		"product": "Oktoberfest",
 		"style": "Oktoberfest",
@@ -470,6 +537,7 @@ beers = [
 		"price": "4.50"
 	},
 	{
+		"id": "068",
 		"brewery": "Sierra Nevada",
 		"product": "Pale Ale",
 		"style": "American Pale Ale",
@@ -477,6 +545,7 @@ beers = [
 		"price": "5.50"
 	},
 	{
+		"id": "069",
 		"brewery": "Sly Fox",
 		"product": "Helles Golden Lager",
 		"style": "American Lager",
@@ -484,6 +553,7 @@ beers = [
 		"price": "6.50"
 	},
 	{
+		"id": "070",
 		"brewery": "Southern Tier",
 		"product": "2X Stout (Nitro)",
 		"style": "Sweet Stout",
@@ -491,6 +561,7 @@ beers = [
 		"price": "7.50"
 	},
 	{
+		"id": "071",
 		"brewery": "Alltech",
 		"product": "Kentucky Bourbon Ale",
 		"style": "English Strong Ale",
@@ -498,6 +569,7 @@ beers = [
 		"price": "6.50"
 	},
 	{
+		"id": "072",
 		"brewery": "Erdinger Weissbrau",
 		"product": "Erdinger Weissbier Dunkel",
 		"style": "Dunkelweizen",
@@ -505,6 +577,7 @@ beers = [
 		"price": "8.00"
 	},
 	{
+		"id": "073",
 		"brewery": "Fat Head's",
 		"product": "Bumble Berry Honey Blueberry Ale",
 		"style": "Fruit",
@@ -512,6 +585,7 @@ beers = [
 		"price": "7.50"
 	},
 	{
+		"id": "074",
 		"brewery": "Fuller Smith & Turner PLC",
 		"product": "Fuller's London Porter",
 		"style": "English Porter",
@@ -519,6 +593,7 @@ beers = [
 		"price": "6.00"
 	},
 	{
+		"id": "075",
 		"brewery": "Penn Brewery",
 		"product": "St. Nikolaus Bock Reserve",
 		"style": "Doppelbock",
@@ -526,6 +601,7 @@ beers = [
 		"price": "7.50"
 	},
 	{
+		"id": "076",
 		"brewery": "Rust Belt",
 		"product": "Rusted River Irish Red",
 		"style": "Irish Red Ale",
@@ -533,6 +609,7 @@ beers = [
 		"price": "8.50"
 	},
 	{
+		"id": "077",
 		"brewery": "Church Brew Works",
 		"product": "Arand The Red / Absolution Ale",
 		"style": "Irish Ale",
@@ -540,6 +617,7 @@ beers = [
 		"price": "3.50"
 	},
 	{
+		"id": "078",
 		"brewery": "Coors",
 		"product": "Coors Light",
 		"style": "Pale Lager",
@@ -547,6 +625,7 @@ beers = [
 		"price": "9.00"
 	},
 	{
+		"id": "079",
 		"brewery": "Helltown",
 		"product": "Mischievous Brown Ale",
 		"style": "English Brown Ale",
@@ -554,6 +633,7 @@ beers = [
 		"price": "5.50"
 	},
 	{
+		"id": "080",
 		"brewery": "North Coast Brewing",
 		"product": "Scrimshaw",
 		"style": "German Pilsner",
@@ -561,6 +641,7 @@ beers = [
 		"price": "5.50"
 	},
 	{
+		"id": "081",
 		"brewery": "Penn Brewery",
 		"product": "Penn Pilsner",
 		"style": "Pilsner / Vienna Lager",
@@ -568,13 +649,15 @@ beers = [
 		"price": "6.50"
 	},
 	{
+		"id": "082",
 		"brewery": "Pivovary Staropramen",
 		"product": "Staropramen Lager",
-		"style": "Czech Pilsener",
+		"style": "Czech Pilsner",
 		"abv": "5.00",
 		"price": "4.50"
 	},
 	{
+		"id": "083",
 		"brewery": "Wells & Young",
 		"product": "Double Chocolate Stout (Nitro)",
 		"style": "Stout",
@@ -582,6 +665,7 @@ beers = [
 		"price": "4.50"
 	},
 	{
+		"id": "084",
 		"brewery": "Labatt",
 		"product": "Blue Light",
 		"style": "Lager",
@@ -589,6 +673,7 @@ beers = [
 		"price": "3.00"
 	},
 	{
+		"id": "085",
 		"brewery": "Leffe",
 		"product": "Blonde",
 		"style": "Belgian Pale Ale",
@@ -596,6 +681,7 @@ beers = [
 		"price": "4.00"
 	},
 	{
+		"id": "086",
 		"brewery": "Rust Belt",
 		"product": "Blast Furnace Blonde",
 		"style": "Blonde Ale",
@@ -603,6 +689,7 @@ beers = [
 		"price": "7.00"
 	},
 	{
+		"id": "087",
 		"brewery": "Stone Brewing Co",
 		"product": "Stone IPA",
 		"style": "American IPA",
@@ -610,6 +697,7 @@ beers = [
 		"price": "6.00"
 	},
 	{
+		"id": "088",
 		"brewery": "Yuengling",
 		"product": "Traditional Lager",
 		"style": "American Amber",
@@ -617,6 +705,7 @@ beers = [
 		"price": "7.00"
 	},
 	{
+		"id": "089",
 		"brewery": "Allagash Brewing Company",
 		"product": "White",
 		"style": "Belgian Style Wheat",
@@ -624,6 +713,7 @@ beers = [
 		"price": "5.50"
 	},
 	{
+		"id": "090",
 		"brewery": "Fat Head's",
 		"product": "Head Hunter",
 		"style": "American IPA",
@@ -631,6 +721,7 @@ beers = [
 		"price": "5.00"
 	},
 	{
+		"id": "091",
 		"brewery": "Lindeman's",
 		"product": "Pomme",
 		"style": "Apple Lambic",
@@ -638,6 +729,7 @@ beers = [
 		"price": "6.50"
 	},
 	{
+		"id": "092",
 		"brewery": "McKenzie's Hard Cider",
 		"product": "Lemon Cider",
 		"style": "Cider",
@@ -645,6 +737,7 @@ beers = [
 		"price": "9.50"
 	},
 	{
+		"id": "093",
 		"brewery": "McKenzie's Hard Cider",
 		"product": "Black Cherry",
 		"style": "Cider",
@@ -652,6 +745,7 @@ beers = [
 		"price": "3.50"
 	},
 	{
+		"id": "094",
 		"brewery": "Penn Brewery",
 		"product": "Dark",
 		"style": "Dunkel",
@@ -659,6 +753,7 @@ beers = [
 		"price": "7.00"
 	},
 	{
+		"id": "095",
 		"brewery": "Weltenburger",
 		"product": "Asam Bock",
 		"style": "Doppelbock",
